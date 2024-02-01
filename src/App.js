4ef215ad8d8514c6bbb79f13fd4c315d270e4363
dinhthanhhai <dthk164@gmail.com>
2024-02-01 18:16:21 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from "./logo.svg";
 import "./App.scss";
+import Nav from "./components/Navigation/Nav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app-container">
+        <Nav />
+        <Routes>
+          <Route path="/news"></Route>
+          <Route path="/contact"></Route>
+          <Route path="/about"></Route>
+          <Route path="/"></Route>
+          <Route path="/login" element={<Login />}></Route>
+
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
