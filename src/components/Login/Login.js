0833b349b765eac5762bc3aa80cd93e4fbf22e07
dinhthanhhai <dthk164@gmail.com>
2024-02-21 23:05:12 +0700

    form login responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,9 +3,9 @@ import "./Login.scss";
 
 const Login = (props) => {
   return (
-    <div className="login-container mt-3">
+    <div className="login-container">
       <div className="container">
-        <div className="row">
+        <div className="row px-3 px-sm-0">
           <div className="content-left col-7 d-none d-sm-block">
             <div className="brand">Login</div>
             <div className="detail">
@@ -13,7 +13,9 @@ const Login = (props) => {
               containing element to wrap site contents.
             </div>
           </div>
+
           <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3">
+            <div className="brand d-sm-none">Login</div>
             <input
               type="text"
               placeholder="Email address or phone number"
@@ -26,7 +28,11 @@ const Login = (props) => {
             ></input>
             <button className="btn btn-primary">Login</button>
             <hr />
-            <span className="text-center">Forgotten your password?</span>
+            <span className="text-center">
+              <a href="#" className="forgot-password">
+                Forgotten your password?
+              </a>
+            </span>
             <div className="text-center">
               <button className="btn btn-success">Create New Account</button>
             </div>
